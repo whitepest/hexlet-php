50eec40a3640cd8ee44723728c63c9abe2afb8be
kozakotomato <silverkin7@gmail.com>
2021-08-15 17:28:10 +0300

    feat: launch in index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo 5 + 10;
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Файл не включается на прямую
+// Он загрузится автоматически благодаря автозагрузке
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
